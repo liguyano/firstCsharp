ce78abe43380c8309b84e4519609b3803b5870b8
liguyano <kurumiwu@foxmail.com>
2022-12-19 11:06:52 +0800

    Initial commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+ static int plus1(int a)
+{
+    return a+1;
+}
+Console.WriteLine("Hello, World!");
+int a= Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(plus1(a));
+
